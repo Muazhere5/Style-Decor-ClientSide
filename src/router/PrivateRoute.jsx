a9e64525fdc4d2d8881cc16d2ce5d7266c277e16
Muazhere5 <muazctg07@gmail.com>
2025-12-21 19:22:44 +0600

    PrivateRoute

---
+++ src/router/PrivateRoute.jsx
@@ -0,0 +1,25 @@
+// src/router/PrivateRoute.jsx
+import { Navigate, useLocation } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+  const location = useLocation();
+
+  if (loading) {
+    return (
+      <div className="loading-indicator">
+        {/* Put your logo loader postimage here */}
+        <span className="loading loading-spinner loading-lg text-primary"></span>
+      </div>
+    );
+  }
+
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+};
+
+export default PrivateRoute;
