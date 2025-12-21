f0ccea9417eaf1cefc64697f7cb078dbd7db6ab0
Muazhere5 <muazctg07@gmail.com>
2025-12-22 04:56:41 +0600

    axiossecure

---
+++ src/hooks/useAxiosSecure.jsx
@@ -0,0 +1,50 @@
+// src/hooks/useAxiosSecure.jsx
+import axios from "axios";
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
+
+const axiosSecure = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+const useAxiosSecure = () => {
+  const { user, logOut } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // REQUEST INTERCEPTOR
+    const requestInterceptor = axiosSecure.interceptors.request.use(
+      config => {
+        const token = localStorage.getItem("styledecor-token");
+        if (token) {
+          config.headers.authorization = `Bearer ${token}`;
+        }
+        return config;
+      },
+      error => Promise.reject(error)
+    );
+
+    // RESPONSE INTERCEPTOR
+    const responseInterceptor = axiosSecure.interceptors.response.use(
+      response => response,
+      async error => {
+        if (error.response?.status === 401 || error.response?.status === 403) {
+          await logOut();
+          localStorage.removeItem("styledecor-token");
+          navigate("/login");
+        }
+        return Promise.reject(error);
+      }
+    );
+
+    return () => {
+      axiosSecure.interceptors.request.eject(requestInterceptor);
+      axiosSecure.interceptors.response.eject(responseInterceptor);
+    };
+  }, [user, logOut, navigate]);
+
+  return axiosSecure;
+};
+
+export default useAxiosSecure;
