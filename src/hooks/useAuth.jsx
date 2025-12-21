333a2b35d58672bbb490de14df85dccc1b6fbc63
Muazhere5 <muazctg07@gmail.com>
2025-12-22 04:54:58 +0600

    useAuth

---
+++ src/hooks/useAuth.jsx
@@ -0,0 +1,15 @@
+// src/hooks/useAuth.jsx
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+
+const useAuth = () => {
+  const auth = useContext(AuthContext);
+
+  if (!auth) {
+    throw new Error("useAuth must be used within AuthProvider");
+  }
+
+  return auth;
+};
+
+export default useAuth;
